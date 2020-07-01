9013cca2bf0ade2df7c52605a41f98e877505269
jorgedq <jorgedq7952@gmail.com>
2020-07-01 18:20:21 -0400

    se ha cambiado los nombres qeu contenian f

--- SearchBugsAutomation/src/tests/SB_40_TotalPriceTest.java
+++ SearchBugsAutomation/src/tests/SB_40_TotalPriceTest.java
@@ -26,7 +26,7 @@ public class SB_40_TotalPriceTest {
 		
   }
   @Test
-  public void f() throws InterruptedException {
+  public void veerifyTotalPriceTest() throws InterruptedException {
 	  boolean codeExists = false;
 		
 		onMonth="4";
